1cd033a1aa182ea0587e34e73247e13661560876
hanif akil <abuhanifakil@gmail.com>
2026-07-13 23:35:46 +0600

    Fix toolbar buttons: add all constructors, ripple backgrounds
    - Add 1-arg and 3-arg constructors to InputView for proper inflation
      on all Android versions
    - Change toolbar button backgrounds from transparent to
      selectableItemBackgroundBorderless for ripple feedback on press
    - Ensures settings, clipboard, and numpad buttons respond to clicks

--- app/src/main/java/rkr/simplekeyboard/inputmethod/latin/InputView.java
+++ app/src/main/java/rkr/simplekeyboard/inputmethod/latin/InputView.java
@@ -20,6 +20,7 @@ package rkr.simplekeyboard.inputmethod.latin;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -46,8 +47,16 @@ public final class InputView extends FrameLayout {
         void onNumPadClicked();
     }
 
+    public InputView(final Context context) {
+        super(context);
+    }
+
     public InputView(final Context context, final AttributeSet attrs) {
         super(context, attrs, 0);
+    }
+
+    public InputView(final Context context, final AttributeSet attrs, final int defStyle) {
+        super(context, attrs, defStyle);
     }
 
     @Override
